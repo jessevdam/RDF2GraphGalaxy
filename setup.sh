7c6cc8316a44388967ed760aaa0e127cbccf4ae7
Jesse c j van Dam <jesse.vandam@wur.nl>
2015-06-29 18:15:14 +0200

    fix setup.sh problem

--- setup.sh
+++ setup.sh
@@ -3,7 +3,9 @@
 PASS=$( openssl rand -hex 32 )
 #setup without the installation of all the dependencies               
 ####################################################
-#ORACLE Database setup...
+#Database setup...
+#echo "DROP DATABASE rdfdb" | sudo -u postgres psql postgres
+#echo "DROP USER rdf" | sudo -u postgres psql postgres
 echo "CREATE USER rdf WITH PASSWORD 'rdf$PASS'" | sudo -u postgres psql postgres
 sudo -u postgres createdb --owner=rdf rdfdb
 ####################################################
@@ -15,7 +17,7 @@ if [ ! -f ../../config/galaxy.ini ]; then
         #Run replace command tobereplaced@email.com
         echo "Type in the email adress that will be used for galaxy admin user:"
         read emailGalaxy
-        sed -e s/tobereplaced@email.com/$emailGalaxy/g ./settings/galaxy.ini.sample | sed -e s/rdf1234/galaxy$PASS/g > ../../config/galaxy.ini
+        sed -e s/tobereplaced@email.com/$emailGalaxy/g ./settings/galaxy.ini.sample | sed -e s/rdf1234/rdf$PASS/g > ../../config/galaxy.ini
 fi
 cp ./settings/datatypes_conf.xml ../../config/
 cp -r settings/static/ ../../static/
