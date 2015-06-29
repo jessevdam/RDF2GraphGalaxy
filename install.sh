9d34198e7f27f4dfe2a9eced64ec2fe93134b177
Jesse c j van Dam <jesse.vandam@wur.nl>
2015-06-29 17:43:10 +0200

    but setup commands in seperate setup file

--- install.sh
+++ install.sh
@@ -29,24 +29,8 @@ select yn in "Yes" "No"; do
                         echo "I am sorry but I am unable to run this under windows..."
                         break
                 fi
-                ####################################################
-                #ORACLE Database setup...
-                sudo apt-get install postgresql postgresql-contrib
-                echo "CREATE USER rdf WITH PASSWORD 'rdf$PASS'" | sudo -u postgres psql postgres
-                sudo -u postgres createdb --owner=rdf rdfdb
-                ####################################################
-                #Galaxy configure scripts
-                cp ./settings/tool_conf.xml ../../config/
-                cp ./settings/datatypes/*.py ../../lib/galaxy/datatypes/
-                #IF file already exists skip
-                if [ ! -f ../../config/galaxy.ini ]; then
-                        #Run replace command tobereplaced@email.com
-                        echo "Type in the email adress that will be used for galaxy admin user:"
-                        read emailGalaxy
-                        sed -e s/tobereplaced@email.com/$emailGalaxy/g ./settings/galaxy.ini.sample | sed -e s/galaxy1234/galaxy$PASS/g > ../../config/galaxy.ini
-                fi
-                cp ./settings/datatypes_conf.xml ../../config/
-                cp -r settings/static/ ../../static/
+
+                ./setup.sh
                 ####################################################
                 #Folder initialization
                 mkdir ./Programs/
