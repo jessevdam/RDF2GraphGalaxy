b67688b5ff85558709bb81b028b4b2b78d1cb0f1
Jesse c j van Dam <jesse.vandam@wur.nl>
2016-03-08 21:05:00 +0100

    added support for recovery from public endpoints

--- 1_recover.sh
+++ 1_recover.sh
@@ -8,12 +8,18 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
+source_select=$1
+input=$2
+extension=$3
+endpoint_adress=$4
+graph=$5
+username=$6 
+password=$7
+output=$8
+simplify=$9
+removeOWL=$10
+isDocker=0
 
-input=$1 
-extension=$2
-output=$3
-simplify=$4
-removeOWL=$5
 if [ "$simplify" = "true" ] ; then
   simplify="--executeSimplify"
 else
@@ -26,15 +32,33 @@ else
   removeOWL=""
 fi
 
-size=$( cat $input | wc -l )
+graph_adress=""
 
-if [ $size -ge 20000000 ] ; then
-  echo "number of lines limited to 20.000.000" 1>&2;
-  echo "nubmer of lines in file $size" 1>&2;
+if [ "$source_select" = "file" ] ; then
+  size=$( cat $input | wc -l )
+  if [[ $isDocker == 0 && $size -ge 20000000 ]] ; then
+    echo "number of lines limited to 20.000.000" 1>&2;
+    echo "nubmer of lines in file $size" 1>&2;
+    exit
+  fi
+  graph_adress=file://$input{$extension}
+else
+  if [[ ("$graph" != "") ]] ; then
+    graph="[$graph]"
+  fi
+  if [[ ("$username" != "") ]] ; then
+    username="$username:$password@"
+  fi
+  graph_adress="$username$endpoint_adress$graph"
+fi
+
+public=$(cat $DIR/checkpubliclist.txt)
+if [[ ($(echo $endpoint_adress | grep -xE $public -) != "") ]] ; then
+  echo "please, do not run RDF2Graph directly on a public endpoint" 1>&2;
   exit
 fi
 
-java -jar $DIR/programs/RDF2Graph/target/RDF2Graph-0.1-jar-with-dependencies.jar temp file://$input{$extension} --all $removeOWL $simplify
+java -jar $DIR/programs/RDF2Graph/target/RDF2Graph-0.1-jar-with-dependencies.jar temp $graph_adress --all $removeOWL $simplify
 tdbquery --loc temp --query $DIR/programs/RDF2Graph/rdfExporter/queries/all.txt --results N3 > $output
-#$DIR/programs/RDF2Graph/rdfExporter temp $output
+$DIR/programs/RDF2Graph/rdfExporter temp $output
 
